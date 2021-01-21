ae64d38ef4d773afb8046abe05e38b8c853eea95
kiserukun <73691576+kiserukun@users.noreply.github.com>
2021-01-21 17:09:50 +0900

    アソシエーショ

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :blogs
+
          validates :name, presence: true
+
 end
--- db/migrate/20210121080654_create_blogs.rb
+++ db/migrate/20210121080654_create_blogs.rb
@@ -2,8 +2,9 @@ class CreateBlogs < ActiveRecord::Migration[6.0]
   def change
     create_table :blogs do |t|
       t.string :title,         null: false, default: ""
-      t.text :text,            null: false, default: ""
-      t.references :user,      foreign_key: true
+      t.text :matter
+      t.references :user, foreign_key: true
+
       t.timestamps
     end
   end
