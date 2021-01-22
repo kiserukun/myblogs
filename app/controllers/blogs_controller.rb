38820c494d856eb28defd70697aaa3bdf8aa80c4
kiserukun <73691576+kiserukun@users.noreply.github.com>
2021-01-22 20:24:15 +0900

    show

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,10 +1,27 @@
 class BlogsController < ApplicationController
   def index
+    @blogs = Blog.all
   end
 
   def new
+    @blog = Blog.new
   end
   
-  def create_table
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title,:matter,:image).merge(user_id: current_user.id)
   end
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,7 +1,7 @@
 class Blog < ApplicationRecord
   validates :title,presence: true
   validates :image,presence: true
-  validates :matter
+  validates :matter,presence: true
 
   belongs_to :user
   has_one_attached :image
